7d8dac03523b8ef7e517ce59e4712c1c6579af37
zhangzhhz <zhangzhhz@gmail.com>
2020-05-05 17:50:57 +0000

    update

--- src/App.js
+++ src/App.js
@@ -2,20 +2,38 @@ import React from 'react';
 import { parse } from 'papaparse';
 
 function App() {
-  const [contacts, setContacts] = React.useState(() => ({email: "fake@gmail.com", name: "Fake"}));
+  const [contacts, setContacts] = React.useState(() => [{ email: "fake@gmail.com", name: "Fake" }]);
+  const [infoList, setInfoList] = React.useState(null);
 
   return (
     <div>
-      <h1 className="text-center text-4x1">Contact Import</h1>
+      <h1 className="text-center text-4xl">Contact Import</h1>
       <div
-        classname={`p-6 my-2 mx-auto max-w-md border-2`}
-        onDragEnter={() => {}}
-        onDragLeave={() => {}}
-        onDragOver={() => {}}
-        onDrop={() => {}}
+        className={`p-6 my-2 mx-auto max-w-md border-2`}
+        onDragEnter={() => { }}
+        onDragLeave={() => { }}
+        onDragOver={(e) => {
+          e.preventDefault();
+        }}
+        onDrop={(e) => {
+          e.preventDefault();
+          console.log(e.dataTransfer);
+          setInfoList(Array.from(e.dataTransfer.files));
+        }}
       >
         DROP HERE
       </div>
+      {infoList === null ? '' : (<div className="text-center">
+        {`You dropped ${infoList.length} files`}
+        {infoList.map((info, idx) => {
+          return (
+            <li key={idx}>
+              {info.name}, {info.size} bytes, {info.type}
+            </li>
+          );
+        })}
+        </div>)
+      }
       <ul>
         {contacts.map(contact => {
           return (
